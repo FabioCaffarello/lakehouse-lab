2d7ea7546d4e216ae4117f9a04ce905ae67764e3
Fabio Caffarello <fabio.caffarello@gmail.com>
2025-04-10 11:36:42 -0300

    docs: improve mkdocs doc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lakehouse Lab
+
+[Lakehouse Lab Documentation](https://fabiocaffarello.github.io/lakehouse-lab/)
+
+Lakehouse Lab is a modular, domain-driven data orchestration platform designed to simulate complex data ingestion workflows. The project integrates multiple languages and tools—including Python, Node.js, Golang, and Docker—to offer a comprehensive solution for testing, development, and integration of data processes.
+
+---
--- scripts/gen_doc_stubs.py
+++ scripts/gen_doc_stubs.py
@@ -38,6 +38,22 @@ def walk_and_process_parent_dir(project_path):
         parent_path = parent_path.parent
 
 
+def render_python_docstring(path: Path):
+    for py_path in path.glob("*.py"):
+        if py_path.name == "__init__.py":
+            continue  # Skip __init__.py
+
+        doc_path = Path("reference", py_path.relative_to(src_root)).with_suffix(".md")
+        doc_normalized_path = str(doc_path).replace(
+            str(path.parent), f"{str(path.parent)}/code_reference"
+        )
+        with mkdocs_gen_files.open(doc_normalized_path, "w") as f:
+            ident = ".".join(py_path.with_suffix("").parts)
+            f.write(f"::: {ident}")
+
+        mkdocs_gen_files.set_edit_path(doc_normalized_path, f"../{py_path}")
+
+
 # Process individual projects
 for path in src_root.rglob("project.json"):
     walk_and_process_parent_dir(path.parent)
@@ -60,3 +76,4 @@ for path in src_root.rglob("project.json"):
         ]
 
         process_patterns(project_path, custom_patterns)
+        render_python_docstring(Path(project["sourceRoot"]))
